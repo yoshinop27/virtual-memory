ad6cf667bf26198131c3f2af9043171a074b1e45
Alexander <alexande2@atanasoff.cs.grinnell.edu>
2025-09-22 14:26:38 -0500

    add relevant library and make sure to exit after SIGSEGV

--- libetsbefriends.c
+++ libetsbefriends.c
@@ -1,37 +1,36 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>  // Added: seed rand() so messages vary each run
 #include <unistd.h>
 
 const char* get_msg(const char* msg[]);
 void seg_handler(int signal, siginfo_t* info, void* ctx);
-const char *messages[] = {
-  "Encouraging message 1",
-  "Encouraging message 2",
-  "Encouraging message 3",
-  "Encouraging message 4",
-  "Encouraging message 5",
-  "Encouraging message 6",
-  "Encouraging message 7",
-  "Encouraging message 8"
-};
+
+const char* messages[] = {"Encouraging message 1", "Encouraging message 2", "Encouraging message 3",
+                          "Encouraging message 4", "Encouraging message 5", "Encouraging message 6",
+                          "Encouraging message 7", "Encouraging message 8"};
 
 // TODO: Implement your signal handling code here!
 __attribute__((constructor)) void init() {
+  srand((unsigned)(time(NULL) ^ getpid()));  // Cite: man 3 srand, time
   struct sigaction sa;
   memset(&sa, 0, sizeof(sa));
   sa.sa_sigaction = seg_handler;
-  sa.sa_flags = SA_SIGINFO;
-  if(sigaction(SIGSEGV, &sa, NULL) != 0) {
+  sa.sa_flags = SA_SIGINFO;  // Cite: man 2 sigaction
+
+  if (sigaction(SIGSEGV, &sa, NULL) != 0) {
     perror("sigaction failed");
     exit(2);
   }
 }
 
-// function handler segmentation faults
-void seg_handler(int signal, siginfo_t* info, void* ctx){
+// function handler for segmentation faults
+void seg_handler(int signal, siginfo_t* info, void* ctx) {
   printf("%s", get_msg(messages));
+  // Why: after a SIGSEGV we must end the program;
+  _exit(0);
 }
 
 // generate random message
@@ -39,4 +38,3 @@ const char* get_msg(const char* msg[]) {
   int idx = rand() % 8;
   return msg[idx];
 }
-
